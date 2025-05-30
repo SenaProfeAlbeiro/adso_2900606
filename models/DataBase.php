477ecdca29db710ef50d55d9a3e029ae80ab61ad
SenaProfeAlbeiro <earamos42@misena.edu.co>
2025-05-30 17:04:16 -0500

    Cambio Cadena Conexión Local

--- models/DataBase.php
+++ models/DataBase.php
@@ -4,28 +4,28 @@
         public static function connection(){
 
             ## Conexión Local
-            // $hostname = "localhost";
-            // $port = "3306";
-            // $database = "db_2900606";
-            // $username = "root";
-            // $password = "";
-			// $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
-			// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			//     return $pdo;
-            // }
-            
-            ## Conexión Azure
-            $hostname = "db-server-app-clean.mysql.database.azure.com";
+            $hostname = "localhost";
             $port = "3306";
             $database = "db_2900606";
-            $username = "admin_db";
-            $password = "database123.";
-            $options = array(
-                PDO::MYSQL_ATTR_SSL_CA => 'assets/_docs_/DigiCertGlobalRootCA.crt.pem'
-            );
-            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $pdo;
-            }            
+            $username = "root";
+            $password = "";
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			    return $pdo;
+            }
+            
+            ## Conexión Azure
+            // $hostname = "db-server-app-clean.mysql.database.azure.com";
+            // $port = "3306";
+            // $database = "db_2900606";
+            // $username = "admin_db";
+            // $password = "database123.";
+            // $options = array(
+            //     PDO::MYSQL_ATTR_SSL_CA => 'assets/_docs_/DigiCertGlobalRootCA.crt.pem'
+            // );
+            // $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
+            //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //     return $pdo;
+            // }            
 		}
 ?>
